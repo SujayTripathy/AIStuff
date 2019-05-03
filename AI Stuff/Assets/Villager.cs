e6518052c771dafda69a1b54bafc439726d07efc
DESKTOP-2U81M9K\Dell <sujay.tripathy@gmail.com>
2019-05-03 09:53:16 -0400

    Initial Commit
    Made a AI which moves to different points periodically. It stops when it reaches those points. Started work on a day/night cycle.

---
+++ AI Stuff/Assets/Villager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Villager : MonoBehaviour
+{
+
+    Vector3 dir;
+    float initial;
+    int point;
+    [SerializeField]
+    GameObject[] points;
+    float movement = 10;
+    float speed = 10;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initial = -movement;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - initial > movement)
+        {
+            initial = Time.time;
+            point = Random.Range(0, points.Length);
+            gameObject.transform.LookAt(points[point].transform);
+            dir = (points[point].transform.position - gameObject.transform.position).normalized;
+            //Debug.DrawLine(gameObject.transform.position, gameObject.transform.position + dir * 10, Color.red, Mathf.Infinity);
+        }
+        if ((gameObject.transform.position - points[point].transform.position).magnitude >= 1) 
+        {
+            
+            gameObject.transform.Translate(dir.normalized * Time.deltaTime*5,Space.World);
+            Debug.DrawLine(gameObject.transform.position, gameObject.transform.position+dir*speed, Color.red, Mathf.Infinity);
+        }
+        else
+        {
+           
+        }
+        
+    }
+}

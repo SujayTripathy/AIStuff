e734ce2806825ce5bae9e19b0631e57430794cf8
DESKTOP-2U81M9K\Dell <sujay.tripathy@gmail.com>
2019-05-03 17:49:05 -0400

    Added day/night functionality.
    No intellisense when using VS. Have downloaded VS 19, and VS Code too and no luck on them either.

--- AI Stuff/Assets/DayNightCycle.cs
+++ AI Stuff/Assets/DayNightCycle.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class DayNightCycle : MonoBehaviour
 {
+    [SerializeField]
+    float time = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(transform.localEulerAngles.x);
+        //time++;
+        transform.Rotate(new Vector3(1, 0, 0));
+        //if (transform.rotation.x ==1)
+        //{
+        //    time = 0;
+        //    transform.eulerAngles = new Vector3(time / 100, transform.eulerAngles.y, transform.eulerAngles.z);
+        //}
     }
 }
